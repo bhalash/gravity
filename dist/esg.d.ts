b2c5107e7fd6444b561a43a9482462ef80e5bc1f
Mark <mark@bhalash.com>
2016-08-28 00:41:22 +0100

    Updated .gitignore.

--- dist/esg.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class ESG {
-    G: number;
-    ESG_EARTH: number;
-    precision(value: number, precision?: number): number;
-    gravity(mass: number, radius: number): number;
-    relative(mass: number, radius: number): number;
-    escape(mass: number, radius: number): number;
-    orbit(mass: number, radius: number): number;
-}
